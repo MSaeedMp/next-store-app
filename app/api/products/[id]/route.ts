9fffffdd7c05686cd625af4caec350255f4cdb13
Saeed Mafipour <m.saeed.mafipour@tum.de>
2025-01-07 14:24:22 +0100

    first commit

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,34 +1,39 @@
 import prisma from "@/utils/db";
-import { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (
-  requeset: NextRequest,
-  { params }: { params: { id: string } }
-) => {
-  if (!requeset?.url)
+export const GET = async (request: NextRequest) => {
+  // Validate the URL
+  if (!request?.url) {
     return NextResponse.json({ error: "Invalid URL" }, { status: 400 });
+  }
 
-  const productId = (await params).id;
+  // Extract product ID from the request's URL
+  const { searchParams } = new URL(request.url);
+  const productId = searchParams.get("id");
 
-  if (!productId)
+  if (!productId) {
     return NextResponse.json(
       { error: "Product ID is required." },
       { status: 400 }
     );
+  }
 
   try {
+    // Fetch the product from the database
     const product = await prisma.product.findUnique({
       where: { id: productId },
     });
-    if (!product)
+
+    if (!product) {
       return NextResponse.json(
         { error: "Product not found!" },
         { status: 404 }
       );
+    }
+
     return NextResponse.json(product);
   } catch (error) {
-    console.error(`Failed to fecth product wih ID ${productId}`, error);
+    console.error(`Failed to fetch product with ID ${productId}`, error);
     return NextResponse.json(
       {
         error: error instanceof Error ? error.message : "Internal Server Error",
